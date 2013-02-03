20fa990124aac2597bb0d45dc517546f4efb55a2
Live session user <ubuntu@ubuntu.(none)>
2013-02-03 15:20:08 +0000

    Added generator files.

---
+++ lib/generators/objvent/install_generator.rb
@@ -0,0 +1,16 @@
+module Objvent
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path("../../templates", __FILE__)
+
+      def copy_initializer
+        template "objvent.rb", "config/initializers/objvent.rb"
+        template "websocket_rails.rb", "config/events.rb"
+      end
+
+      def show_readme
+        readme "README" if behavior == :invoke
+      end
+    end
+  end
+end
--- lib/objvent/rails/engine.rb
+++ lib/objvent/rails/engine.rb
@@ -1,6 +1,10 @@
 module Objvent
   module Rails
     class Engine < ::Rails::Engine
+
+      config.autoload_paths += [File.expand_path("../../lib", __FILE__)]
+      
+
     end
   end
 end
